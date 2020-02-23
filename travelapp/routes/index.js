bce03afa50f2605e3e0bb59f4ef7f5701c1278c3
Pedro Meaud <meaud23@gmail.com>
2020-02-23 18:09:20 +0000

    views updated

--- travelapp/routes/index.js
+++ travelapp/routes/index.js
@@ -5,7 +5,7 @@ const router = new Router();
 const routeGuard = require('./../middleware/route-guard');
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Hello World!' });
+  res.render('index', { title: `Friend's Trip` });
 });
 
 router.get('/logged', routeGuard, (req, res, next) => {
