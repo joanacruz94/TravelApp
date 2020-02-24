ae10edc2b24a71fa83149b6debacd201aa4841f5
Pedro Meaud <meaud23@gmail.com>
2020-02-24 17:18:52 +0000

    map views

--- travelapp/routes/social.js
+++ travelapp/routes/social.js
@@ -5,31 +5,47 @@ const router = new Router();
 const Post = require('./../models/post');
 
 router.get('/profile', (req, res, next) => {
-    const user = req.user;
+  const user = req.user;
 
-    Post.find({'postedBy' : user._id})
-    .then((posts) => {
-        const data = {
-            posts,
-            user
-        };
-        res.render('user/profile', data);
+  Post.find({ postedBy: user._id })
+    .then(posts => {
+      const data = {
+        posts,
+        user
+      };
+      res.render('user/profile', data);
     })
-    .catch((error) => {
-        next(error);
+    .catch(error => {
+      next(error);
     });
 });
 
 router.get('/friends', (req, res, next) => {
-    res.render('user/friends');
+  res.render('user/friends');
 });
 
 router.get('/update', (req, res, next) => {
-    res.render('user/edit');
+  res.render('user/edit');
 });
 
 router.get('/update', (req, res, next) => {
-    res.render('user/map');
+  res.render('user/map');
+});
+
+router.get('/map', (req, res, next) => {
+  const user = req.user;
+
+  Post.find({ postedBy: user._id })
+    .then(posts => {
+      const data = [];
+      posts.map(value =>
+        data.push({ lat: value.location.coordinates[0], lng: value.location.coordinates[1] })
+      );
+      res.render('user/map', { data });
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 module.exports = router;
