2664c588ebae125de3026a9110358c810cbc0dc2
Joana Cruz <joanamartadacruz@gmail.com>
2020-02-27 03:34:40 +0000

    translation working

--- travelapp/routes/utilities.js
+++ travelapp/routes/utilities.js
@@ -2,108 +2,48 @@
 
 const { Router } = require('express');
 const router = new Router();
-const {TranslationServiceClient} = require('@google-cloud/translate');
+const {Translate} = require('@google-cloud/translate').v2;
+const translate = new Translate();
+const target = 'en';
+let translated = '';
 
-const projectId = 'geoproject-268614';
-const location = 'global';
-const text = 'Hello World';
 
-// Instantiates a client
-const translationClient = new TranslationServiceClient();
-async function translateText() {
-
-  // Construct request
-  const request = {
-    parent: `projects/${projectId}/locations/${location}`,
-    contents: [text],
-    mimeType: 'text/plain', // mime types: text/plain, text/html
-    sourceLanguageCode: 'en',
-    targetLanguageCode: 'pt'
-  };
-
-  try {
-    // Run request
-    const [response] = await translationClient.translateText(request);
-
-    for (const translation of response.translations) {
-    console.log('ola');
-      console.log(`Translation: ${translation.translatedText}`);
-    }
-  } catch (error) {
-    console.log(error);
-
-    console.error(error.details);
-  }
+async function translateText(text) {
+  let [translations] = await translate.translate(text, target);
+  translations = Array.isArray(translations) ? translations : [translations];
+  translations.forEach((translation, i) => {
+    console.log(`${text[i]} => (${target}) ${translation}`);
+    translated = translation;
+  });
 }
 
+router.get('/', (req, res, next) => {
+  res.render('utilities/main');
+});
 
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+router.get('/translation', (req, res, next) => {
+  console.log("NEEEEEDD", translated);
+  res.render('utilities/translation', { translated });
+});
 
+router.post('/translation', (req, res, next) => {
+    const text = req.body.text;
+    
+    translateText(text);
+
+    res.render('utilities/translation', { translated });
+});
 
 router.get('/recognition', (req, res, next) => {
     res.render('utilities/recognition');
 });
 
-router.get('/translation', (req, res, next) => {
-    //translateText();
-
-
-// using SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
-
-const msg = {
-  to: 'joanamartadacruz@gmail.com',
-  from: 'joanamartadacruz@gmail.com',
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-};
-
-console.log(msg);
-sgMail.send(msg);
-
-
-res.render('utilities/translation');
-});
-
-router.post('/translation', (req, res, next) => {
-    const text = req.body.text;
-
-});
 
 
 
 
-router.get('/', function (req, res) {
-  sendEmail({
-       toAddress: 'joanamartadacruz@gmail.com',
-       subject: 'Email from SMTP sever',
-       data: {  // data to view template, you can access as - user.name
-          name: 'Arjun PHP',
-          message: 'Welcome to arjunphp.com'
-       },
-       htmlPath: "welcome.pug"
-   }).then(() => {
-     return res.send('Email has been sent!');
-   }).catch((error) => {
-     return res.send('There was an error sending the email');
-   })
-});
 
-const sendEmail = function(mailOptionsObject) {
 
-  const msg = {
-  to: mailOptionsObject.toAddress,
-  from: 'joanamartadacruz@gmail.com',
-  subject: mailOptionsObject.subject,
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>'
-  };
-
-const status = sgMail.send(msg)
-return status;
-
-};
 
 module.exports = router;
