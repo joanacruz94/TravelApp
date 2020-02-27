ab75a6fd6b03675a31a393d6898a7779c9717b63
Joana Cruz <joanamartadacruz@gmail.com>
2020-02-27 21:53:47 +0000

    adding stuff to error and friends page

--- passport-configuration.js
+++ passport-configuration.js
@@ -83,7 +83,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/authentication/google-callback"
+    callbackURL: "https://localhost:3000/authentication/google-callback"
   },
   (accessToken, refreshToken, profile, callback) => {
     User.findOne({
