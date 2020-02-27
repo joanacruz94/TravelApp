1c64d349b016589a03fa357ec5a917c2780b0403
Pedro Meaud <meaud23@gmail.com>
2020-02-27 15:30:00 +0000

    fix

--- routes/utilities.js
+++ routes/utilities.js
@@ -2,12 +2,11 @@
 
 const { Router } = require('express');
 const router = new Router();
-const {Translate} = require('@google-cloud/translate').v2;
+const { Translate } = require('@google-cloud/translate').v2;
 const vision = require('@google-cloud/vision');
 const translate = new Translate();
 const target = 'en';
 let translated = '';
-
 
 async function translateText(text) {
   let [translations] = await translate.translate(text, target);
@@ -19,12 +18,8 @@ async function translateText(text) {
 }
 
 async function quickstart() {
-  // Imports the Google Cloud client library
-
-  // Creates a client
   const client = new vision.ImageAnnotatorClient();
 
-  // Performs label detection on the image file
   const [result] = await client.labelDetection('./cat.jpg');
   const labels = result.labelAnnotations;
   console.log('Labels:');
@@ -35,34 +30,24 @@ router.get('/', (req, res, next) => {
   res.render('utilities/main');
 });
 
-
 router.get('/translation', (req, res, next) => {
   res.render('utilities/translation');
 });
 
 router.post('/translation', (req, res, next) => {
-    const text = req.body.text;
-    
-    translateText(text).
-    then(() =>{
-      console.log('hhhhjjjjh');
-      console.log('hgggggggggggggggg', translated);
+  const text = req.body.text;
+
+  translateText(text)
+    .then(() => {
+      console.log(translated);
       res.render('utilities/translation', { translated });
     })
     .catch(console.error);
-
 });
 
 router.get('/recognition', (req, res, next) => {
-    quickstart();
-    //res.render('utilities/recognition');
+  quickstart();
+  //res.render('utilities/recognition');
 });
-
-
-
-
-
-
-
 
 module.exports = router;
